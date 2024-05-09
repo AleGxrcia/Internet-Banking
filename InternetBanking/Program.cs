1cd736774ef33f8cfb06e69ee7b7f8376b6a3c29
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-05-09 17:14:23 -0400

    Add app settings

--- InternetBanking/Program.cs
+++ InternetBanking/Program.cs
@@ -1,9 +1,43 @@
+using InternetBanking.Infrastructure.Persistence;
+using InternetBanking.Infrastructure.Identity;
+using InternetBanking.Core.Application;
+using InternetBanking.Infrastructure.Identity.Entities;
+using InternetBanking.Infrastructure.Identity.Seeds;
+using Microsoft.AspNetCore.Identity;
+using InternetBanking.WebApp.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
+builder.Services.AddPersistenceInfrastructure(builder.Configuration);
+builder.Services.AddIdentityInfrastructure(builder.Configuration);
+builder.Services.AddApplicationLayer();
+builder.Services.AddScoped<LoginAuthorize>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddTransient<ValidateUserSession, ValidateUserSession>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+        await DefaultRoles.SeedAsync(userManager, roleManager);
+        await DefaultAdminUser.SeedAsync(userManager, roleManager);
+        await DefaultClientUser.SeedAsync(userManager, roleManager);
+    }
+    catch (Exception ex)
+    {
+
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -13,15 +47,17 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Auth}/{action=Login}/{id?}");
 
 app.Run();
