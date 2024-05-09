1f2b7731d71b4f861d382f777d7289a616821791
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-05-09 13:27:38 -0400

    Add service and generic repository

---
+++ InternetBanking.Core.Application/Interfaces/Services/IGenericService.cs
@@ -0,0 +1,14 @@
+﻿namespace InternetBanking.Core.Application.Interfaces.Services
+{
+    public interface IGenericService<SaveViewModel, ViewModel, Model>
+        where SaveViewModel : class
+        where ViewModel : class
+        where Model : class
+    {
+        Task Update(SaveViewModel vm, int id);
+        Task<SaveViewModel> Add(SaveViewModel vm);
+        Task Delete(int id);
+        Task<SaveViewModel> GetByIdSaveViewModel(int id);
+        Task<List<ViewModel>> GetAllViewModel();
+    }
+}
