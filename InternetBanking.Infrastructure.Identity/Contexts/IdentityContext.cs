07dca9379df439a9ab4533483b7eb3c5e6a686a1
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-05-09 15:41:43 -0400

    Initial identity configuration

---
+++ InternetBanking.Infrastructure.Identity/Contexts/IdentityContext.cs
@@ -0,0 +1,40 @@
+﻿using InternetBanking.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternetBanking.Infrastructure.Identity.Contexts
+{
+    public class IdentityContext : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //FLUENT API
+            modelBuilder.HasDefaultSchema("Identity");
+
+            modelBuilder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable(name: "Users");
+            });
+
+            modelBuilder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Roles");
+            });
+
+            modelBuilder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable(name: "UserRoles");
+            });
+
+            modelBuilder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable(name: "UserLogins");
+            });
+        }
+    }
+}
