6c4da287f646f0fb5d4d010b758fd0bdc5be957c
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-05-09 16:58:50 -0400

    Add user seeds with differents roles

---
+++ InternetBanking.Infrastructure.Identity/Seeds/DefaultAdminUser.cs
@@ -0,0 +1,34 @@
+﻿using InternetBanking.Core.Application.Enums;
+using InternetBanking.Infrastructure.Identity.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace InternetBanking.Infrastructure.Identity.Seeds
+{
+    public static class DefaultAdminUser
+    {
+        public static async Task SeedAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            ApplicationUser defaultUser = new();
+            defaultUser.UserName = "adminuser";
+            defaultUser.Email = "adminuser@email.com";
+            defaultUser.FirstName = "John";
+            defaultUser.LastName = "Doe";
+            defaultUser.IdNumber = "873123123";
+            defaultUser.PhoneNumber = "8093821234";
+            defaultUser.IsActive = true;
+            defaultUser.EmailConfirmed = true;
+            defaultUser.PhoneNumberConfirmed = true;
+
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "123Pa$$word!");
+                    await userManager.AddToRoleAsync(defaultUser, Roles.Admin.ToString());
+                }
+            }
+
+        }
+    }
+}
